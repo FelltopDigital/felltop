6d911e334a9fa995fd9af97286b5ad6510b04a6d
Tom Walsh <tom@felltopdigital.com>
2025-01-25 21:35:55 +0000

    adds head element

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,8 +1,11 @@
-import Link from "next/link";
+import Head from 'next/head';
 
-export default function NotFound() {
+export default function Home() {
   return (
     <main className="h-screen flex items-center justify-center bg-lime-950/50">
+      <Head>
+        <title>Felltop Digital | 404 Page</title>
+      </Head>
       <div className="text-center block w-full max-w-[260px] md:max-w-full">
         <h1 className="text-2xl">Felltop Digital</h1>
         <p className="text-lime-200 text-sm">Page not found, please head back <Link className="underline" href="/">Home</Link>.</p>
