12a3eeff3219394d01b9775b97d252bb84941edf
FelltopDigital <tom@felltopdigital.com>
2025-01-26 16:39:46 +0000

    Update not-found.js

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,15 +1,14 @@
-import Head from 'next/head';
-
 export default function Home() {
   return (
     <main className="h-screen flex items-center justify-center bg-lime-950/50">
-      <Head>
-        <title>Felltop Digital | 404 Page</title>
-      </Head>
       <div className="text-center block w-full max-w-[260px] md:max-w-full">
         <h1 className="text-2xl">Felltop Digital</h1>
         <p className="text-lime-200 text-sm">Page not found, please head back <Link className="underline" href="/">Home</Link>.</p>
       </div>
     </main>
   );
+}
+
+export const metadata = {
+  title: 'Felltop Digital | 404 Page',
 }
