17bea34f62ff0b5547c744858b0d4520fe3faff0
FelltopDigital <tom@felltopdigital.com>
2025-01-26 16:40:00 +0000

    Update not-found.js

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="h-screen flex items-center justify-center bg-lime-950/50">
       <div className="text-center block w-full max-w-[260px] md:max-w-full">
-        <h1 className="text-2xl">Felltop Digital</h1>
+        <h1 className="text-2xl text-lime-950">Felltop Digital</h1>
         <p className="text-lime-200 text-sm">Page not found, please head back <Link className="underline" href="/">Home</Link>.</p>
       </div>
     </main>
